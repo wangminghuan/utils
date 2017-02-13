34b18ad6ad62465859fef4ba0691c2652c72d1f2
wangminghuan <m.h.wang@foxmail.com>
2017-02-13 23:35:04 +0800

    finish ajax

--- util.js
+++ util.js
@@ -145,7 +145,7 @@ String.prototype._trim = function(){
 /**
  * @module  extend  
  * @description child(子集，多，默认参数) 拷贝继承 father(父集，少，用户参数),返回一个新child
- *  深度复制，子集的修改不会影响父集，依赖isEmptyObjec函数
+ *  深度复制，子集的修改不会影响父集，依赖isEmptyObject,isArray函数
  *  主要完成属性为object时参数的继承，而非重新赋值。
  */ 
 var extend = function(parent, child){
@@ -168,18 +168,43 @@ var extend = function(parent, child){
 	return child;
 };
 
-var jsonp=function(){
-
+var jsonp = function() {
+    var url = "http://192.168.120.104:8080/searchsuggest_14.do?inputbox=7&cityid=1&catid=0";
+    var callback = "callback" + Math.random().toString().substring(2, 6);
+    window[callback] = function(data) {
+            console.log(data.w[0].k)
+        }
+        //window[callback],就是把动态变化的callback名（字符串）挂载到了window下，与直接命名
+        //无区别，但是window[变量]中可以存放一个变量，这是"."运算无法做到的。
+    url += "&callback=" + callback;
+    var script = document.createElement('script');
+    script.setAttribute("type", "text/javascript");
+    script.src = url;
+    document.body.appendChild(script);
 };
 
-/*
-ajax工作步骤
-1. 创建XMLHttpRequest对象
-2. 同服务器建立联系，open方法
-3. 向服务器发送请求，send方法
-4. 接收服务器返回请求内容，onreadystatechange
+
+/**
+ * @module  ajax 
+ * @description ajax原生js封装
+   *参数说明:
+   **method: 请求方式:GET/POST,默认值:'GET';
+   **url:    发送请求的地址, 默认值: 当前页地址;
+   **data:   string,json;
+   **async:  是否异步:true/false,默认值:true;
+   **cache:  是否缓存：true/false,默认值:true;
+   **contentType: HTTP头信息，默认值：'application/x-www-form-urlencoded';
+   **success: 请求成功后的回调函数;
+   **error:   请求失败后的回调函数;
  */
-var ajax_0 = function(opts) {
+/*
+**ajax工作步骤
+**1. 创建XMLHttpRequest对象
+**2. 同服务器建立联系，open方法
+**3. 向服务器发送请求，send方法
+**4. 接收服务器返回请求内容，onreadystatechange
+ */
+var ajax = function(opts) {
         var defaults = {
             method: "GET",
             url: "",
@@ -196,7 +221,7 @@ var ajax_0 = function(opts) {
 
         //处理用户输入的data数据
 
-        if (typeof defaults.data == 'object') {
+        if (typeof defaults.data == 'object' && ! Object.prototype.toString.call(defaults.data) == "[object Array]") {
             var dataStr = "";
             for (var k in defaults.data) {
                 dataStr += encodeURIComponent(k) + "=" + encodeURIComponent(defaults.data[k]) + "&"
@@ -208,7 +233,8 @@ var ajax_0 = function(opts) {
 
             //设置cache ,cache为false时设置随机数，防止缓存
             defaults.cache = defaults.cache? "": "&"+(new Date()).getTime();
-            //设置异步
+            
+            //GET方式下将data拼接到url中进行传递
             if (defaults.method == "GET" && (defaults.data||defaults.cache)) {
                 defaults.url += "?" + defaults.data+ defaults.cache;
             }
@@ -218,18 +244,35 @@ var ajax_0 = function(opts) {
 
             //2. 同服务器建立联系，open方法
             xhr.open(defaults.method, defaults.url, defaults.async)
-                //3. 向服务器发送请求，send方法
+            
+            //3. 向服务器发送请求，send方法
             if (defaults.method == "GET") {
                 xhr.send()
             } else {
-                xhr.setHttpRequestHeader('Content-Type', defaults.contentType)
+                xhr.setRequestHeader('Content-Type', defaults.contentType);
+                //提交的数据格式，默认application/x-www-form-urlencoded
                 xhr.send(defaults.data);
             }
+            
             //4. 接收服务器返回请求内容，onreadystatechange
+            
+            /* *onreadystatechange事件
+			* readyState属性：请求状态
+			* 0	（初始化）还没有调用open()方法
+			* 1	（载入）已调用send()方法，正在发送请求
+			* 2	（载入完成）send()方法完成，已收到全部响应内容
+			* 3	（解析）正在解析响应内容
+			* 4	（完成）响应内容解析完成，可以在客户端调用了
+			* status属性：服务器(请求资源)的状态,http状态码
+
+			* 返回的内容
+			* responseText：返回以文本形式存放的内容
+			* responseXML：返回XML形式的内容
+			*/
             xhr.onreadystatechange = function() {
                 if (xhr.readyState == 4) {
                     if (xhr.status == 200) {
-                        defaults.sucess(JSON.parse(xhr.responseText))
+                        defaults.sucess(xhr.responseText)
                     } else {
                         defaults.error(xhr.status)
                     }
@@ -237,146 +280,4 @@ var ajax_0 = function(opts) {
             }
 };
 
-
-
-
-/**
- * @module  ajax 
- * @description 简易ajax实现原理
- */ 
-var ajax2=function (method, url, data, success) {
-	var xhr = null;
-	try {
-		xhr = new XMLHttpRequest();
-	} catch (e) {
-		xhr = new ActiveXObject('Microsoft.XMLHTTP');
-	}
-	
-	if (method == 'get' && data) {
-		url += '?' + data;
-	}
-	/*
-			open方法
-			参数
-				1.打开方式
-				2.地址
-				3.是否异步
-					异步:非阻塞 前面的代码不会影响后面代码的执行
-					同步:阻塞 前面的代码会影响后面代码的执行
-		*/
-	xhr.open(method,url,true);
-
-/*	2.post
-        理论上无限制
-        enctype : 提交的数据格式，默认application/x-www-form-urlencoded*/
-	if (method == 'get') {
-		xhr.send();
-	} else {
-		xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
-		xhr.send(data);
-	}
-	
-	/*onreadystatechange事件
-	readyState属性：请求状态
-	0	（初始化）还没有调用open()方法
-	1	（载入）已调用send()方法，正在发送请求
-	2	（载入完成）send()方法完成，已收到全部响应内容
-	3	（解析）正在解析响应内容
-	4	（完成）响应内容解析完成，可以在客户端调用了
-	status属性：服务器(请求资源)的状态
-	返回的内容
-	responseText：返回以文本形式存放的内容
-	responseXML：返回XML形式的内容*/
-   //等待服务器返回内容
-		/*
-			readyState : ajax工作状态
-			responseText : ajax请求返回的内容就被存放到这个属性下面
-			on readystate change : 当readyState改变的时候触发
-			status : 服务器状态，http状态码
-		*/
-	xhr.onreadystatechange = function() {
-		
-		if ( xhr.readyState == 4 ) {
-			if ( xhr.status == 200 ) {
-				success && success(xhr.responseText);
-			} else {
-				alert('出错了,Err：' + xhr.status);
-			}
-		}
-		
-	}
-};
-
-/**
- * @module  ajax 
- * @description ajax原生js封装
-   *参数说明:
-   **method: 请求方式:GET/POST,默认值:'GET';
-   **url:    发送请求的地址, 默认值: 当前页地址;
-   **data:   string,json;
-   **async:  是否异步:true/false,默认值:true;
-   **cache:  是否缓存：true/false,默认值:true;
-   **contentType: HTTP头信息，默认值：'application/x-www-form-urlencoded';
-   **success: 请求成功后的回调函数;
-   **error:   请求失败后的回调函数;
- */
- var ajax = function (opts) {
-    //设置默认参数
-    var defaults = {
-        method: 'GET',
-        url: '',
-        data: '',
-        async: true,
-        cache: true,
-        contentType: 'application/x-www-form-urlencoded',
-        success: function() {},
-        error: function() {}
-    };
-
-    //默认参数继承用户参数 
-    for (var key in opts) {
-        defaults[key] = opts[key];
-    }
-
-    //对数据进行处理
-    if (typeof defaults.data === 'object') { //处理 data
-        var str = '';
-        for (var key in defaults.data) {
-            str += key + '=' + defaults.data[key] + '&';
-        }
-        defaults.data = str.substring(0, str.length - 1);
-    }
-
-       defaults.method = defaults.method.toUpperCase(); //处理 method
-
-       defaults.cache = defaults.cache ? '' : '&' + new Date().getTime(); //处理 cache
-
-    if (defaults.method === 'GET' && (defaults.data || defaults.cache)){
-       defaults.url += '?' + defaults.data + defaults.cache; //处理 url    
-    }
-    //开始编写ajax
-    //1.创建ajax对象
-    var oXhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-    
-    //2.和服务器建立联系，告诉服务器你要取什么文件
-    oXhr.open(defaults.method, defaults.url, defaults.async);
-    
-    //3.发送请求
-    if (defaults.method === 'GET')
-        oXhr.send(null);
-    else {
-        oXhr.setRequestHeader("Content-Type", defaults.contentType);
-        oXhr.send(defaults.data);
-    }
-    //4.等待服务器回应
-    oXhr.onreadystatechange = function() {
-        if (oXhr.readyState === 4) {
-            if (oXhr.status === 200)
-                defaults.success.call(oXhr, oXhr.responseText);
-            else {
-                defaults.error();
-            }
-        }
-    };
-}
 
