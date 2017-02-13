ecd08b6f00d75b9cbbf0122a3b1f42b01a1d481f
wangminghuan <wangminghuan@58ganji.com>
2017-02-13 20:23:37 +0800

    modify 20170213

--- util.js
+++ util.js
@@ -168,6 +168,77 @@ var extend = function(parent, child){
 	return child;
 };
 
+var jsonp=function(){
+
+};
+
+/*
+ajax工作步骤
+1. 创建XMLHttpRequest对象
+2. 同服务器建立联系，open方法
+3. 向服务器发送请求，send方法
+4. 接收服务器返回请求内容，onreadystatechange
+ */
+var ajax_0 = function(opts) {
+        var defaults = {
+            method: "GET",
+            url: "",
+            data: '',
+            async: true,
+            cache: true,
+            contentType: 'application/x-www-form-urlencoded',
+            sucess: function() {},
+            error: function() {}
+        };
+        for (var key in opts) {
+            defaults[key] = opts[key]
+        }
+
+        //处理用户输入的data数据
+
+        if (typeof defaults.data == 'object') {
+            var dataStr = "";
+            for (var k in defaults.data) {
+                dataStr += encodeURIComponent(k) + "=" + encodeURIComponent(defaults.data[k]) + "&"
+                }
+            defaults.data = dataStr.substring(0, dataStr.length - 1)
+            }
+            //将请求方式改为大写
+            defaults.method = defaults.method.toUpperCase();
+
+            //设置cache ,cache为false时设置随机数，防止缓存
+            defaults.cache = defaults.cache? "": "&"+(new Date()).getTime();
+            //设置异步
+            if (defaults.method == "GET" && (defaults.data||defaults.cache)) {
+                defaults.url += "?" + defaults.data+ defaults.cache;
+            }
+
+            //1. 创建XMLHttpRequest对象
+            var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+
+            //2. 同服务器建立联系，open方法
+            xhr.open(defaults.method, defaults.url, defaults.async)
+                //3. 向服务器发送请求，send方法
+            if (defaults.method == "GET") {
+                xhr.send()
+            } else {
+                xhr.setHttpRequestHeader('Content-Type', defaults.contentType)
+                xhr.send(defaults.data);
+            }
+            //4. 接收服务器返回请求内容，onreadystatechange
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        defaults.sucess(JSON.parse(xhr.responseText))
+                    } else {
+                        defaults.error(xhr.status)
+                    }
+                }
+            }
+};
+
+
+
 
 /**
  * @module  ajax 
@@ -294,7 +365,7 @@ var ajax2=function (method, url, data, success) {
     if (defaults.method === 'GET')
         oXhr.send(null);
     else {
-        oXhr.setRequestHeader("Content-type", defaults.contentType);
+        oXhr.setRequestHeader("Content-Type", defaults.contentType);
         oXhr.send(defaults.data);
     }
     //4.等待服务器回应
